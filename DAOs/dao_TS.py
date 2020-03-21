64cf86c6b15c4515b78e88459a9c749e03d0f876
Jainel M. Torres Santos <jainel.torres@upr.edu>
2020-03-21 19:05:19 -0400

    feat(DAOs): finished implementation of tests & refactor dao_TS.py
    BREAKING CHANGE:

--- DAOs/dao_TS.py
+++ DAOs/dao_TS.py
@@ -3,64 +3,64 @@ from schema_DB import *
 import datetime
 
 def post_create_doc_DAO (**docatr):
-    authorDoc = []
-    for author in docatr["authorList"]:
-        auth = Author(author_FN = author.first_name, author_LN = author.last_name, author_email = author.email, 
-        author_faculty = author.faculty)
-        authorDoc.append(auth)
-    actorDoc = []
-    for actor in docatr["actorList"]:
-        act = Actor(actor_FN = actor.first_name, actor_LN = actor.last_name, role = actor.role)
-        actorDoc.append(act)
-    timelineDoc = []
-    for tl in docatr["timelineList"]:
-        timel = Timeline(event = tl.event, eventDate = tl.eventDate)
-        timelineDoc.append(timel)
-    sectionDoc = []
-    for sec in docatr["sectionList"]:
-        secdoc = Section(secTitle = sec.title, content = sec.content)
-        timelineDoc.append(secdoc)
-    doc1 = DocumentCase(title = docatr["title"], description = docatr["des"],
-    incidentDate = docatr["inDate"], creationDate = docatr["crDate"],
-    tagsDoc = docatr["tags"], infrasDocList = docatr["infraList"], damageDocList = docatr["damList"],
-    location = docatr["locList"], author = authorDoc, actor = actorDoc, section = sectionDoc,
-    timeline = timelineDoc)
+    # authorDoc = []
+    # for author in docatr["authorList"]:
+    #     auth = Author(author_FN = author.first_name, author_LN = author.last_name, author_email = author.email, 
+    #     author_faculty = author.faculty)
+    #     authorDoc.append(auth)
+    # actorDoc = []
+    # for actor in docatr["actorList"]:
+    #     act = Actor(actor_FN = actor.first_name, actor_LN = actor.last_name, role = actor.role)
+    #     actorDoc.append(act)
+    # timelineDoc = []
+    # for tl in docatr["timelineList"]:
+    #     timel = Timeline(event = tl.event, eventDate = tl.eventDate)
+    #     timelineDoc.append(timel)
+    # sectionDoc = []
+    # for sec in docatr["sectionList"]:
+    #     secdoc = Section(secTitle = sec.title, content = sec.content)
+    #     timelineDoc.append(secdoc)
+    doc1 = DocumentCase(creatoriD = docatr["creatoriD"],title = docatr["title"], description = docatr["description"],
+    incidentDate = docatr["incidentDate"], creationDate = docatr["creationDate"],
+    tagsDoc = docatr["tagsDoc"], infrasDocList = docatr["infrasDocList"], damageDocList = docatr["damageDocList"],
+    location = docatr["location"], author = docatr["author"], actor = docatr["actor"], 
+    section = docatr["section"], timeline = docatr["timeline"])
     doc1.save()
     
-def get_me(idcollab):
-    return Collaborator.objects.get(email = idcollab)
+def get_me(email_collab):
+    return Collaborator.objects.get(email = email_collab)
 
-def get_doc(idcolab):
-    return DocumentCase.objects.filter(title = idcolab)
+def get_doc(title_doc):
+    return DocumentCase.objects.filter(title = title_doc)
 
-def put_doc_title(docid, title):
-    DocumentCase.objects(title = docid).update_one(set__title = title)
+def put_doc_title(title_doc, title):
+    DocumentCase.objects(title = title_doc).update_one(set__title = title)
 
-def put_doc_des(docid, des):
-    DocumentCase.objects(title = docid).update_one(set__description = des)
+def put_doc_des(title_doc, des):
+    DocumentCase.objects(title = title_doc).update_one(set__description = des)
 
-def put_doc_timeline( docid, timeline):
-    DocumentCase.objects(title = docid).update_one(set__timeline = timeline)
+def put_doc_timeline( title_doc, timeline):
+    DocumentCase.objects(title = title_doc).update_one(set__timeline = timeline)
 
-def put_doc_section(docid, sec):
-    DocumentCase.objects(title = docid).update_one(set__section = sec)
+def put_doc_section(title_doc, sec):
+    DocumentCase.objects(title = title_doc).update_one(set__section = sec)
 
-def put_doc_damageType(docid, damType):
-    DocumentCase.objects(title = docid).update_one(set__damageDocList = damType)
+def put_doc_damageType(title_doc, damType):
+    DocumentCase.objects(title = title_doc).update_one(set__damageDocList = damType)
    
-def put_doc_infrasType(docid,infrasType):
-    DocumentCase.objects(title = docid).update_one(set__infrasDocList = infrasType)
+def put_doc_infrasType(title_doc,infrasType):
+    DocumentCase.objects(title = title_doc).update_one(set__infrasDocList = infrasType)
 
-def put_doc_locations(docid,loc):
-    DocumentCase.objects(title = docid).update_one(set__location = loc)
+def put_doc_locations(title_doc,loc):
+    DocumentCase.objects(title = title_doc).update_one(set__location = loc)
 
-def put_doc_actors(docid, actors):
-    DocumentCase.objects(title = docid).update_one(set__actor = actors)
+def put_doc_actors(title_doc, actors):
+    DocumentCase.objects(title = title_doc).update_one(set__actor = actors)
 
-def pit_doc_authors(docid, authors):
-    DocumentCase.objects(title = docid).update_one(set__author = authors)
+def put_doc_authors(title_doc, authors):
+    DocumentCase.objects(title = title_doc).update_one(set__author = authors)
 
-def pit_doc_tags(docid, tags):
-    DocumentCase.objects(title = docid).update_one(set__tagsDoc = tags)
+def put_doc_tags(title_doc, tags):
+    DocumentCase.objects(title = title_doc).update_one(set__tagsDoc = tags)
     
 
--- DAOs/dao_test.py
+++ DAOs/dao_test.py
@@ -3,30 +3,119 @@ from schema_DB import *
 from dao_TS import *
 import datetime
 
-#returns collaborator object
-collab = "aurora.black@upr.edu"
-get = get_me(collab)
-print(get.first_name)
+# Testing Dao get collaborator 
+# collab = "aurora.black@upr.edu"
+# get = get_me(collab)
+# print(get.first_name)
 
-#return list doc object
-doc = "The great Flooding"
-get_doc = get_doc(doc)
-for x in get_doc: 
-    print(x.title)
 
+# tesing DAO get list docs
+# doc = "The great Flooding"
+# get_doc = get_doc(doc)
+# for x in get_doc: 
+#     print(x.title)
+
+#testing DAO update title
 change_title = "The final countdown"
-put_doc_title(doc, change_title)
-update_title = DocumentCase.objects.get(title = "The final countdown")
-print(update_title.title)
+# put_doc_title(doc, change_title)
+# update_title = DocumentCase.objects.get(title = "The final countdown")
+# print(update_title.title)
 
-change_des = "uff, it was bad"
-put_doc_des(change_title, change_des)
-update_des = DocumentCase.objects.get(title = "The final countdown")
-update_des.reload()
-print(update_des.description)
-
-# change_timel = "uff, it was bad"
+#testing DAO update description
+# change_des = "uff, it was bad"
 # put_doc_des(change_title, change_des)
 # update_des = DocumentCase.objects.get(title = "The final countdown")
 # update_des.reload()
 # print(update_des.description)
+
+#testing DAO update timeline
+# change_timel1 = Timeline(event = "event1", eventDate = datetime.datetime(2016,9,9))
+# change_timel2 = Timeline(event = "event2", eventDate = datetime.datetime(2015,5,5))
+# change_list_timel = [change_timel1, change_timel2]
+# put_doc_timeline(change_title, change_list_timel)
+# update_timel = DocumentCase.objects.get(title = "The final countdown")
+# update_timel.reload()
+# for x in range(0, len(update_timel.timeline)):
+#     print(update_timel.timeline[x].event, update_timel.timeline[x].eventDate)
+
+# testing DAO update section
+# change_section1 = Section(secTitle = "Title", content = "content1")
+# change_section2 = Section(secTitle = "title2", content = "content2")
+# change_list_section = [change_section1, change_section2]
+# put_doc_section(change_title, change_list_section)
+# update_section = DocumentCase.objects.get(title = "The final countdown")
+# update_section.reload()
+# for x in range(0, len(update_section.section)):
+#     print(update_section.section[x].secTitle, update_section.section[x].content)
+
+# testing DAO update damagetype
+# change_list_damage = ["change_damage1", "change_damage2"]
+# put_doc_damageType(change_title, change_list_damage)
+# update_damage = DocumentCase.objects.get(title = "The final countdown")
+# update_damage.reload()
+# for x in range(0, len(update_damage.damageDocList)):
+#     print(update_damage.damageDocList[x])
+
+# testing DAO update infrastructureType
+# change_list_infra = ["infra1", "infra2"]
+# put_doc_infrasType(change_title, change_list_infra)
+# update_infra = DocumentCase.objects.get(title = "The final countdown")
+# update_infra.reload()
+# for x in range(0, len(update_infra.infrasDocList)):
+#     print(update_infra.infrasDocList[x])
+
+# testing DAO update locations
+# change_list_loc = ["loc1", "loc2"]
+# put_doc_locations(change_title, change_list_loc)
+# update_loc = DocumentCase.objects.get(title = "The final countdown")
+# update_loc.reload()
+# for x in range(0, len(update_loc.location)):
+#     print(update_loc.location[x])
+
+# testing DAO update actor
+# change_actor1 = Actor(actor_FN = "name1", actor_LN = "lname1", role = "role1")
+# change_actor2 = Actor(actor_FN = "name2", actor_LN = "lname2", role = "role2")
+# change_list_actor = [change_actor1, change_actor2]
+# put_doc_actors(change_title, change_list_actor)
+# update_actor = DocumentCase.objects.get(title = "The final countdown")
+# update_actor.reload()
+# for x in range(0, len(update_actor.actor)):
+#     print(update_actor.actor[x].actor_FN, update_actor.actor[x].actor_LN, update_actor.actor[x].role)
+
+#testing DAO update author
+# change_author1 = Author(author_FN = "auname1", author_LN = "aulname1", 
+# author_email = "auemail1@upr.edu", author_faculty = "aufaculty1")
+# change_author2 = Author(author_FN = "auname2", author_LN = "aulname2", 
+# author_email = "auemail2@upr.edu",author_faculty = "aufaculty2")
+# change_list_author = [change_author1, change_author2]
+# put_doc_authors(change_title, change_list_author)
+# update_author = DocumentCase.objects.get(title = "The final countdown")
+# update_author.reload()
+# for x in range(0, len(update_author.author)):
+#     print(update_author.author[x].author_FN, update_author.author[x].author_LN, 
+#     update_author.author[x].author_email, update_author.author[x].author_faculty)
+
+# testing DAO update tags
+# change_list_tags = ["tag1", "tag2"]
+# put_doc_tags(change_title, change_list_tags)
+# update_tag = DocumentCase.objects.get(title = "The final countdown")
+# update_tag.reload()
+# for x in range(0, len(update_tag.tagsDoc)):
+#     print(update_tag.tagsDoc[x])
+
+# testing DAO post doc
+auth1 = Author(author_FN = "author.first_name1", author_LN = "author.last_name1", 
+author_email = "author.email1@upr.edu", author_faculty = "author.faculty1")
+auth2 = Author(author_FN = "author.first_name2", author_LN = "author.last_name2", 
+author_email = "author.email2@upr.edu", author_faculty = "author.faculty2")
+authlist = [auth1,auth2]
+act1 = Actor(actor_FN = "actor.first_name", actor_LN = "actor.last_name", role = "actor.role")
+actlist = [act1]
+timel = Timeline(event = "tl.event", eventDate = datetime.datetime(2010,2,2))
+timelineDoc = [timel]
+secdoc = Section(secTitle = "sec.title", content = "sec.content")
+sectionDoc = [secdoc]
+post_create_doc_DAO(creatoriD = "s",author = authlist,actor = actlist, timeline = timelineDoc,
+section = sectionDoc, title = "title10", description = "des10",
+incidentDate = datetime.datetime(2010,2,2),creationDate = datetime.datetime(2010,2,2),
+tagsDoc = ["...,.","mkdjcioe"], infrasDocList = ["infras10"], damageDocList = ["dam10"], location = ["loc10"])
